f11ec0c9e755c5d18f59a53819e1e83e997d0100
TooSeriuz <frongs@rfcx.org>
2025-03-11 19:20:51 +0700

    Add placeholder functions for geofence module

---
+++ src/geofence/geofence.rs
@@ -0,0 +1,14 @@
+mod geofence {
+    fn should_geofence() -> bool {
+        true
+    }
+
+    fn roll_up_labels_to_first_matching_level() {
+
+    }
+
+    fn geofence_classification() {
+
+    }
+}
+
