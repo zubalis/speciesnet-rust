1bc1d65be0a67e129c06b8e0365e1eb91f66a568
Bhattarapong Somwong <numbbutt34685@gmail.com>
2025-03-24 19:48:18 +0700

    New separate cli crate

---
+++ speciesnet-cli/src/file_extension.rs
@@ -0,0 +1,2 @@
+/// Supported list of image extensions for speciesnet cli.
+pub const SUPPORTED_IMAGE_EXTENSIONS: [&str; 5] = ["jpg", "jpeg", "png", "tiff", "webp"]
---
+++ speciesnet-cli/src/main.rs
@@ -0,0 +1,117 @@
+use std::path::PathBuf;
+
+use clap::{Args, CommandFactory, Parser, error::ErrorKind};
+use log::debug;
+
+mod file_extension;
+
+#[derive(Debug, Args)]
+#[group(required = true, multiple = false)]
+struct InputType {
+    /// Path of instances.json file input.
+    #[arg(long)]
+    instances_json: Option<PathBuf>,
+    /// Path of filepaths to be added into the model.
+    #[arg(long)]
+    filepaths: Vec<PathBuf>,
+    /// Path of the filepaths.txt file.
+    #[arg(long)]
+    filepaths_txt: Option<PathBuf>,
+    /// Path of folders to run inference on.
+    #[arg(long)]
+    folders: Vec<PathBuf>,
+    /// Path of folders.txt file.
+    #[arg(long)]
+    folders_txt: Option<PathBuf>,
+}
+
+#[derive(Debug, Args)]
+#[group(required = false, multiple = false)]
+pub struct AdditionalConfiguration {
+    /// Path of detections.json file to put in the model.
+    #[arg(long)]
+    detections_json: Option<PathBuf>,
+    /// Path of classifications.json file to put in the model.
+    #[arg(long)]
+    classifications_json: Option<PathBuf>,
+}
+
+#[derive(Debug, Args)]
+#[group(required = false, multiple = true)]
+pub struct RunType {
+    /// Running only the detector.
+    #[arg(long)]
+    detector_only: bool,
+    /// Running only the classifier (requires detections-json).
+    #[arg(long)]
+    classifier_only: bool,
+    /// Running only the ensembler (requires detections-json and classifications-json).
+    #[arg(long)]
+    ensemble_only: bool,
+    /// Enables geofence while running the ensembler.
+    #[arg(long)]
+    geofence: bool,
+}
+
+#[derive(Debug, Parser)]
+pub struct CliArguments {
+    #[command(flatten)]
+    input_type: InputType,
+    #[command(flatten)]
+    run_type: RunType,
+    #[command(flatten)]
+    additional_config: AdditionalConfiguration,
+    /// The path of the detector model.
+    #[arg(long)]
+    detector_model: PathBuf,
+    /// Output predictions.json file path of the predictions result.
+    #[arg(long)]
+    predictions_json: PathBuf,
+}
+
+fn main() -> anyhow::Result<()> {
+    env_logger::init();
+
+    let args = CliArguments::parse();
+    let mut cmd = CliArguments::command();
+
+    // Stops the run if predictions-json exists.
+    if args.predictions_json.exists() {
+        cmd.error(
+            ErrorKind::ValueValidation,
+            format!(
+                "Predictions file at {:?} already exists.",
+                args.predictions_json.display()
+            ),
+        )
+        .exit();
+    }
+
+    // Classifier can only be run when detections-json is provided.
+    if args.run_type.classifier_only && args.additional_config.detections_json.is_none() {
+        cmd.error(
+            ErrorKind::MissingRequiredArgument,
+            "When --classifier_only is set, --detections-json must be provided.",
+        )
+        .exit();
+    }
+
+    // Ensemble can only be run when detections-json and classifications-json is provided.
+    if args.run_type.ensemble_only
+        && (args.additional_config.detections_json.is_none()
+            || args.additional_config.classifications_json.is_none())
+    {
+        cmd.error(
+            ErrorKind::MissingRequiredArgument,
+            "When --ensemble-only is set, --classifications-json and --detections-json must be provided."
+        ).exit();
+    }
+
+    // Parse the input files into list of files.
+    
+
+
+    debug!("args is {:?}", args);
+
+    Ok(())
+}
