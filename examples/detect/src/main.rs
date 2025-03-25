d075ead9caa91520a80869153bb129955dcc7f6f
Bhattarapong Somwong <numbbutt34685@gmail.com>
2025-03-25 21:18:40 +0700

    Fixed the examples to be able to run

--- examples/detect/src/main.rs
+++ examples/detect/src/main.rs
@@ -14,7 +14,7 @@ fn main() -> anyhow::Result<()> {
     let image_path = "../../assets/images/african_elephants.jpg";
     let speciesnet = SpeciesNet::new("../../assets/model/md_v5a.0.0_traced.pt")?;
 
-    let results = speciesnet.detect(image_path)?;
+    let results = speciesnet.detect(&[image_path.into()])?;
     let mut loaded_image = ImageReader::open(image_path)?.decode()?.to_rgb8();
     let image_width = loaded_image.width();
     let image_height = loaded_image.height();
@@ -23,25 +23,36 @@ fn main() -> anyhow::Result<()> {
     println!("width {}", image_width);
     println!("height {}", image_height);
 
-    let rects = results
-        .iter()
-        .map(|d| {
-            let (x1, y1, x2, y2) = d.bounding_box().as_xyxy_bounding_box();
-            Rect::at(x1 as i32, y1 as i32).of_size((x2 - x1) as u32, (y2 - y1) as u32)
-        })
-        .collect::<Vec<_>>();
+    let mut rectangles: Vec<Rect> = Vec::new();
+    for result in &results {
+        if let Some(detections) = result.detections() {
+            for detection in detections {
+                let (x1, y1, x2, y2) = detection.bounding_box().as_xyxy_bounding_box();
+                let scaled_x1: i32 = (x1 * loaded_image.width() as f64) as i32;
+                let scaled_y1: i32 = (y1 * loaded_image.height() as f64) as i32;
+                let scaled_x2: i32 = (x2 * loaded_image.width() as f64) as i32;
+                let scaled_y2: i32 = (y2 * loaded_image.height() as f64) as i32;
 
+                rectangles.push(Rect::at(scaled_x1, scaled_y1).of_size(
+                    (scaled_x2 - scaled_x1) as u32,
+                    (scaled_y2 - scaled_y1) as u32,
+                ));
+            }
+        }
+    }
+
+    let detections = results.first().unwrap().detections().as_ref().unwrap();
     let color = Rgb::from_slice(&[255, 0, 125]);
-    for (i, rect) in rects.iter().enumerate() {
-        draw_hollow_rect_mut(&mut loaded_image, *rect, *color);
+    for (rectangle, result) in std::iter::zip(rectangles, detections) {
+        draw_hollow_rect_mut(&mut loaded_image, rectangle, *color);
         draw_text_mut(
             &mut loaded_image,
             *color,
-            rect.left(),
-            rect.top(),
+            rectangle.left(),
+            rectangle.top(),
             16.0,
             &font,
-            format!("{:?}", results.get(i).unwrap().confidence()).as_str(),
+            format!("{:?}", result.confidence()).as_str(),
         )
     }
 
