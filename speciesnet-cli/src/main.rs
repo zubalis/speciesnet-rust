9c47c4b68036fcd2d03fcb6c289d37fd715b5545
Bhattarapong Somwong <numbbutt34685@gmail.com>
2025-05-15 16:18:45 +0700

    Documentation for cli crate

--- speciesnet-cli/src/main.rs
+++ speciesnet-cli/src/main.rs
@@ -1,3 +1,71 @@
+//! ## SpeciesNet CLI
+//!
+//! `speciesnet-cli` is a CLI for running a cameratrapai ensemble written in Rust.
+//!
+//! ### Installation
+//!
+//! #### Cargo
+//!
+//! ```bash
+//! cargo install --git https://github.com/zubalis/speciesnet-rust.git --path speciesnet-cli
+//! ```
+//!
+//! ### Cargo features
+//!
+//! - `download-model`, enabled by default, downloads the [ONNX](https://onnx.ai)-converted detector and classifier model automatically to be used, you will have to manually pass the path to the extracted model folder in order to run the program if this feature is turned off.
+//!
+//! ### Usage
+//!
+//! The CLI is designed to be pretty similar to how [google/cameratrapai](https://github.com/google/cameratrapai) is, except there are some differences as we're working towards the python equivalent.
+//!
+//! - The input instance file only support `filepath`, `country`, and `admin1_region` keys.
+//! - The classifier output only support the `classes`, and `scores` key.
+//! - Other keys of `predictions` are still not supported on both reading from and writing to files.
+//! - The Rust version does not override or edit an existing `predictions.json` file, if one is found when supplied using `--predictions-json`, CLI will error saying the file already existed.
+//! - The CLI flag `--country` and `--admin1-region` currently does nothing to the input.
+//!
+//! below is the examples of running the ensemble using speciesnet compared to cameratrapai.
+//!
+//! #### Running only the detector
+//!
+//! ```bash
+//! # cameratrapai.
+//! python3 -m speciesnet.scripts.run_model --instance_json ./instance.json --predictions_json ./predictions.json --detector-only
+//!
+//! # speciesnet-rust.
+//! speciesnet-cli --instance-json ./instance.json --predictions_json ./predictions.json --detector-only
+//! ```
+//!
+//! #### Running only the classifier
+//!
+//! ```bash
+//! # cameratrapai.
+//! python3 -m speciesnet.scripts.run_model --instance_json ./instance.json --predictions_json ./predictions.json --classifier-only
+//!
+//! # speciesnet-rust.
+//! speciesnet-cli --instance-json ./instance.json --predictions_json ./predictions.json --classifier-only
+//! ```
+//!
+//! #### Running only the ensemble
+//!
+//! ```bash
+//! # cameratrapai.
+//! python3 -m speciesnet.scripts.run_model --instance_json ./instance.json --predictions_json ./predictions.json --ensemble-only
+//!
+//! # speciesnet-rust.
+//! speciesnet-cli --instance-json ./instance.json --predictions_json ./predictions.json --ensemble-only
+//! ```
+//!
+//! #### Running the whole inference pipeline
+//!
+//! ```bash
+//! # cameratrapai.
+//! python3 -m speciesnet.scripts.run_model --instance_json ./instance.json --predictions_json ./predictions.json
+//!
+//! # speciesnet-rust.
+//! speciesnet-cli --instance-json ./instance.json --predictions_json ./predictions.json
+//! ```
+
 use std::{fs::File, io::BufWriter, path::PathBuf};
 
 use clap::{Args, CommandFactory, Parser, error::ErrorKind};
